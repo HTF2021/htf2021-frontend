41b980fd7571ce0ddbc50af4af8dc77bfab2b63c
julesDecockFlexso <jules.decock@flexso.com>
2021-10-27 16:24:59 +0200

    Link buttons, wijzig beginscherm

--- webapp/controller/cluedo.controller.js
+++ webapp/controller/cluedo.controller.js
@@ -21,6 +21,9 @@ sap.ui.define([
 					this.getView().setModel(new JSONModel(oData), 'cluedoModel');
 					let grondplan = "https://htf-2021.herokuapp.com" + this.getView().getModel('cluedoModel').getData().grondplannen[1].url;
 					this.getView().byId("grondplanImg").setProperty("src", grondplan);
+
+					let startPlan = "https://htf-2021.herokuapp.com" + this.getView().getModel('cluedoModel').getData().grondplannen[0].url;
+					this.getView().byId("startImage").setProperty("src", startPlan);
 				}).catch(()=>{
 					MessageToast.show("Could not load game data.");
 				});
@@ -43,6 +46,8 @@ sap.ui.define([
 			onStartPress: function (evt) {
 				var startButton = this.getView().byId("start");
 				if (startButton.getText() == "Start") {
+					this.getView().byId("startImage").setVisible(false);
+					this.getView().byId("startTitle").setVisible(false);
 					startButton.setText("Restart");
 					startButton.setIcon("sap-icon://restart");
 					startButton.setType("Reject");
@@ -57,6 +62,12 @@ sap.ui.define([
 					this.getView().byId("studeerkamerButton").setVisible(true);
 					this.getView().byId("zitkamerButton").setVisible(true);
 					this.getView().byId("grondplanImg").setVisible(true);
+
+					this.getView().byId("wapen").setVisible(true);
+					this.getView().byId("dader").setVisible(true);
+					this.getView().byId("kamer").setVisible(true);
+					this.getView().byId("valideer").setVisible(true);
+
 				} else {
 					startButton.setText("Start");
 					startButton.setIcon("sap-icon://begin");
@@ -92,21 +103,21 @@ sap.ui.define([
 						console.log(oData); // Log answer (testing)
 						if(oData.wapen){
 							// TO TEST: Set wapen guess correct
-							//this.getView().ById('wapenIcon').setProperty("src", "sap-icon://accept");
+							this.getView().byId('wapenIcon').setProperty("src", "sap-icon://accept");
 						} else {
-							//this.getView().ById('wapenIcon').setProperty("src", "sap-icon://decline");
+							this.getView().byId('wapenIcon').setProperty("src", "sap-icon://decline");
 						}
 						if(oData.dader){
 							// TO TEST: Set dader guess correct
-							//this.getView().ById('daderIcon').setProperty("src", "sap-icon://accept");
+							this.getView().byId('daderIcon').setProperty("src", "sap-icon://accept");
 						} else {
-							//this.getView().ById('daderIcon').setProperty("src", "sap-icon://decline");
+							this.getView().byId('daderIcon').setProperty("src", "sap-icon://decline");
 						}
 						if(oData.kamer){
 							// TO TEST: Set kamer guess correct
-							//this.getView().ById('kamerIcon').setProperty("src", "sap-icon://accept");
+							this.getView().byId('kamerIcon').setProperty("src", "sap-icon://accept");
 						} else {
-							//this.getView().ById('kamerIcon').setProperty("src", "sap-icon://decline");
+							this.getView().byId('kamerIcon').setProperty("src", "sap-icon://decline");
 						}
 					}).catch(()=>{
 						MessageToast.show("Could check the answer. Please try again.");
@@ -114,32 +125,140 @@ sap.ui.define([
 				}
 			},
 			// TODO: On kamer select: select corresponding kamer in dropdown 
-			onBalzaalPress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onBalzaalPress: function () {
+				MessageToast.show("Balzaal");
+				this.getView().byId("kamer").setSelectedKey("0");
+				this.getView().byId("kamer").setValue("Balzaal");
+
+				this.getView().byId("balzaalButton").setType("Accept");
+				this.getView().byId("bibliotheekButton").setType("Reject");
+				this.getView().byId("biljartkamerButton").setType("Reject");
+				this.getView().byId("eetkamerButton").setType("Reject");
+				this.getView().byId("halButton").setType("Reject");
+				this.getView().byId("keukenButton").setType("Reject");
+				this.getView().byId("serreButton").setType("Reject");
+				this.getView().byId("studeerkamerButton").setType("Reject");
+				this.getView().byId("zitkamerButton").setType("Reject");
 			},
-			onBibliotheekPress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onBibliotheekPress: function () {
+				MessageToast.show("Bibliotheek");
+				this.getView().byId("kamer").setSelectedKey("1");
+				this.getView().byId("kamer").setValue("Bibliotheek");
+
+				this.getView().byId("balzaalButton").setType("Reject");
+				this.getView().byId("bibliotheekButton").setType("Accept");
+				this.getView().byId("biljartkamerButton").setType("Reject");
+				this.getView().byId("eetkamerButton").setType("Reject");
+				this.getView().byId("halButton").setType("Reject");
+				this.getView().byId("keukenButton").setType("Reject");
+				this.getView().byId("serreButton").setType("Reject");
+				this.getView().byId("studeerkamerButton").setType("Reject");
+				this.getView().byId("zitkamerButton").setType("Reject");
 			},
-			onBiljartkamerPress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onBiljartkamerPress: function () {
+				MessageToast.show("Biljartkamer");
+				this.getView().byId("kamer").setSelectedKey("2");
+				this.getView().byId("kamer").setValue("Biljartkamer");
+
+				this.getView().byId("balzaalButton").setType("Reject");
+				this.getView().byId("bibliotheekButton").setType("Reject");
+				this.getView().byId("biljartkamerButton").setType("Accept");
+				this.getView().byId("eetkamerButton").setType("Reject");
+				this.getView().byId("halButton").setType("Reject");
+				this.getView().byId("keukenButton").setType("Reject");
+				this.getView().byId("serreButton").setType("Reject");
+				this.getView().byId("studeerkamerButton").setType("Reject");
+				this.getView().byId("zitkamerButton").setType("Reject");
 			},
-			onEetkamerPress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onEetkamerPress: function () {
+				MessageToast.show("Eetkamer");
+				this.getView().byId("kamer").setSelectedKey("3");
+				this.getView().byId("kamer").setValue("Eetkamer");
+
+				this.getView().byId("balzaalButton").setType("Reject");
+				this.getView().byId("bibliotheekButton").setType("Reject");
+				this.getView().byId("biljartkamerButton").setType("Reject");
+				this.getView().byId("eetkamerButton").setType("Accept");
+				this.getView().byId("halButton").setType("Reject");
+				this.getView().byId("keukenButton").setType("Reject");
+				this.getView().byId("serreButton").setType("Reject");
+				this.getView().byId("studeerkamerButton").setType("Reject");
+				this.getView().byId("zitkamerButton").setType("Reject");
 			},
-			onHalPress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onHalPress: function () {
+				MessageToast.show("Hal");
+				this.getView().byId("kamer").setSelectedKey("4");
+				this.getView().byId("kamer").setValue("Hal");
+				
+				this.getView().byId("balzaalButton").setType("Reject");
+				this.getView().byId("bibliotheekButton").setType("Reject");
+				this.getView().byId("biljartkamerButton").setType("Reject");
+				this.getView().byId("eetkamerButton").setType("Reject");
+				this.getView().byId("halButton").setType("Accept");
+				this.getView().byId("keukenButton").setType("Reject");
+				this.getView().byId("serreButton").setType("Reject");
+				this.getView().byId("studeerkamerButton").setType("Reject");
+				this.getView().byId("zitkamerButton").setType("Reject");
 			},
-			onKeukenPress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onKeukenPress: function () {
+				MessageToast.show("Keuken");
+				this.getView().byId("kamer").setSelectedKey("5");
+				this.getView().byId("kamer").setValue("Keuken");
+
+				this.getView().byId("balzaalButton").setType("Reject");
+				this.getView().byId("bibliotheekButton").setType("Reject");
+				this.getView().byId("biljartkamerButton").setType("Reject");
+				this.getView().byId("eetkamerButton").setType("Reject");
+				this.getView().byId("halButton").setType("Reject");
+				this.getView().byId("keukenButton").setType("Accept");
+				this.getView().byId("serreButton").setType("Reject");
+				this.getView().byId("studeerkamerButton").setType("Reject");
+				this.getView().byId("zitkamerButton").setType("Reject");
 			},
-			onSerrePress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onSerrePress: function () {
+				MessageToast.show("Serre");
+				this.getView().byId("kamer").setSelectedKey("6");
+				this.getView().byId("kamer").setValue("Serre");
+
+				this.getView().byId("balzaalButton").setType("Reject");
+				this.getView().byId("bibliotheekButton").setType("Reject");
+				this.getView().byId("biljartkamerButton").setType("Reject");
+				this.getView().byId("eetkamerButton").setType("Reject");
+				this.getView().byId("halButton").setType("Reject");
+				this.getView().byId("keukenButton").setType("Reject");
+				this.getView().byId("serreButton").setType("Accept");
+				this.getView().byId("studeerkamerButton").setType("Reject");
+				this.getView().byId("zitkamerButton").setType("Reject");
 			},
-			onStudeerkamerPress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onStudeerkamerPress: function () {
+				MessageToast.show("Studeerkamer");
+				this.getView().byId("kamer").setSelectedKey("7");
+				this.getView().byId("kamer").setValue("Studeerkamer");
+
+				this.getView().byId("balzaalButton").setType("Reject");
+				this.getView().byId("bibliotheekButton").setType("Reject");
+				this.getView().byId("biljartkamerButton").setType("Reject");
+				this.getView().byId("eetkamerButton").setType("Reject");
+				this.getView().byId("halButton").setType("Reject");
+				this.getView().byId("keukenButton").setType("Reject");
+				this.getView().byId("serreButton").setType("Reject");
+				this.getView().byId("studeerkamerButton").setType("Accept");
+				this.getView().byId("zitkamerButton").setType("Reject");
 			},
-			onZitkamerPress: function (evt) {
-				MessageToast.show(evt.getSource().getId() + " Pressed");
+			onZitkamerPress: function () {
+				MessageToast.show("Zitkamer");
+				this.getView().byId("kamer").setSelectedKey("8");
+				this.getView().byId("kamer").setValue("Zitkamer");
+				
+				this.getView().byId("balzaalButton").setType("Reject");
+				this.getView().byId("bibliotheekButton").setType("Reject");
+				this.getView().byId("biljartkamerButton").setType("Reject");
+				this.getView().byId("eetkamerButton").setType("Reject");
+				this.getView().byId("halButton").setType("Reject");
+				this.getView().byId("keukenButton").setType("Reject");
+				this.getView().byId("serreButton").setType("Reject");
+				this.getView().byId("studeerkamerButton").setType("Reject");
+				this.getView().byId("zitkamerButton").setType("Accept");
 			}
 		});
 	});
